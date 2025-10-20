cddedab5652330e52f1aa61f9ff4dc382e46b0d2
angw <angw.verse@gmail.com>
2025-10-20 22:22:00 +0700

    hí

---
+++ script.js
@@ -0,0 +1,36 @@
+/* FinisherHeader */
+new FinisherHeader({
+  "count": 30,
+  "size": {
+    "min": 1,
+    "max": 48,
+    "pulse": 0.1
+  },
+  "speed": {
+    "x": {
+      "min": 0,
+      "max": 0.4
+    },
+    "y": {
+      "min": 0,
+      "max": 0.4
+    }
+  },
+  "colors": {
+    "background": "#0038bc",
+    "particles": [
+      "#87ddff",
+      "#e3e7d3",
+      "#b9fff8"
+    ]
+  },
+  "blending": "none",
+  "opacity": {
+    "center": 0,
+    "edge": 0.7
+  },
+  "skew": 0,
+  "shapes": [
+    "c"
+  ]
+});
